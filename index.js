7a8a3a0637da24ab69358847dbbc90c19d963bff
wasifmiraz <wasifmiraz@mail.com>
2018-08-30 22:27:20 +0600

    access token done

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const passport require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('./config/keys.js');
 const app =express();
 
-passport.use( new GoogleStrategy());
+passport.use( new GoogleStrategy({
+
+    clientID:Keys.googleClientID,
+    clientSecret:Keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+
+})
+
+);
+
+app.get('/auth/google',
+ passport.authenticate('google',
+{
+    scope:['profile', 'email']
+
+})
+
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 app.listen(process.env.PORT || 8080 ,function(){
